03b84af5f8c6902bcc719481233e228bf5b266eb
SammriddhGupta <sammriddhgupta@gmail.com>
2023-10-16 22:56:45 +1100

    fixed responsiveness, added text

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from langchain_helper import generate_team_name
 import streamlit as st
 
-
 page_bg_img = '''
 <style>
-
 [ data-testid="stAppViewContainer"] {
-background-image: url("https://img.freepik.com/free-photo/office-supplies-with-keyboard-gray-desk_23-2148052624.jpg?w=2000&t=st=1697123051~exp=1697123651~hmac=b1e4ef7975df9d6b938d1102f394f492a74c576b73b28d4ee7d8fd6e0b5966a6");
-
-background-size: cover;
+    background-image: url('https://images.unsplash.com/photo-1488998427799-e3362cec87c3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80');
+    background-size: cover; 
+    background-position: center center;
+    background-repeat: no-repeat; 
+    background-attachment: fixed;
 }
 </style>
 '''
@@ -16,13 +16,14 @@ background-size: cover;
 st.markdown(page_bg_img, unsafe_allow_html=True)
 
 st.title("Hackathon Team Name Generator")
+st.subheader("Save precious time and get hacking faster!")
 
 company_name = st.text_input("Company Name")
 theme_name = st.text_input("Theme of the hack")
 problem_name = st.text_input("Problem Statement")
-idea_name = st.text_input("Idea(if any yet)")
+idea_name = st.text_input("Your Idea (if any yet)")
 
-if st.button("Generate Team Names"):
+if st.button("Generate 20 Team Names"):
     team_name = generate_team_name(company_name, theme_name, problem_name, idea_name)
 
     st.subheader("Here are some awesome team names:")
