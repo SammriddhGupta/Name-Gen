80c4de4dfc1244c39fba753a73214b284158c883
SammriddhGupta <sammriddhgupta@gmail.com>
2024-02-03 23:07:27 +1100

    feat: remove bg img

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
 from langchain_helper import generate_team_name
 import streamlit as st
 
-page_bg_img = '''
-<style>
-[ data-testid="stAppViewContainer"] {
-    background-image: url('https://images.unsplash.com/photo-1488998427799-e3362cec87c3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80');
-    background-size: cover; 
-    background-position: center center;
-    background-repeat: no-repeat; 
-    background-attachment: fixed;
-}
-</style>
-'''
-
 st.markdown(page_bg_img, unsafe_allow_html=True)
 
 st.title("Hackathon Team Name Generator")
